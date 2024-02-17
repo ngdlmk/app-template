1174f5b313d47ccc6b32628c91362e341b0888aa
Kamil Doğan <kamildogan@outlook.com>
2024-02-17 19:21:59 +0300

    add android product flavors

--- App.tsx
+++ App.tsx
@@ -8,8 +8,10 @@
 import React from 'react';
 import {Button, SafeAreaView, Text} from 'react-native';
 import crashlytics from '@react-native-firebase/crashlytics';
+import Config from 'react-native-config';
 
 function App(): React.JSX.Element {
+  console.log(Config)
   return (
     <SafeAreaView style={{flex: 1}}>
       <Text style={{fontSize: 38}}>Super App</Text>
